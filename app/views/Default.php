0b50ff8da553fd9e9d0d1ec24074a12e920f2afb
Vinicius Baroni Soares <vinibaronisoares@gmail.com>
2017-05-22 22:39:06 -0300

    Adicionado o favicon às páginas do sistema base

--- app/views/Default.php
+++ app/views/Default.php
@@ -7,6 +7,8 @@
 	<meta name="author" content="Vinicius Baroni Soares">
 	<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="assets/styles/default.css">
+	<link rel="shortcut icon" type="image/png" href="assets/images/favicon.png">
+	<link rel="apple-touch-icon" href="assets/images/favicon.png" />
 </head>
 <body>
 	<span id="maintitle">
