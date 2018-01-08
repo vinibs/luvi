c201d14b6790a4f45e8509167c5f357da475e666
Vinicius Baroni Soares <vinibaronisoares@gmail.com>
2018-01-08 19:59:17 -0300

    Alterados comentários de instruções relativas à definição de rotas

--- app/Routes.php
+++ app/Routes.php
@@ -1,11 +1,14 @@
 <?php defined('INITIALIZED') OR exit('You cannot access this file directly');
 /*
  * Padrão para a escrita de rotas:
- * $route['URL'] = 'Controller/Method';
+ * Router::define('URL', 'Controller/Method');
  *
- * // Use '?' to indicate that the method needs one or more parameters
- * $route['URL/?'] = 'Controller/Method'; 
+ * // Use '{var_name}' to indicate that the method needs a parameter identified by 'var_name' in the array
+ * Router::define('URL/{id}', 'Controller/Method');
+ * Router::define('URL/{name}/show', 'Controller/Method');
  *
- * OBS: As rotas ao fim do arquivo sobrescrevem as do início, portanto rotas com parâmetros devem vir
- * depois de rotas sem parâmetros para funcionar corretamente.
+ * OBS: A primeira rota válida será sempre a rota utilizada, portanto se forem definidas mais de uma rota para a mesma
+ * URL, as que vierem após a primeira não serão utilizadas.
+ *
  */
+
