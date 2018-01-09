32fd417c4bdfd9f0e19d5fef466f580103b5374b
Vinicius Baroni Soares <vinibaronisoares@gmail.com>
2018-01-09 15:32:10 -0300

    Alterado mecanismo e padrão para definir seções em templates

--- app/system/core/ViewManager.php
+++ app/system/core/ViewManager.php
@@ -23,7 +23,7 @@ class ViewManager
          *      @extends('[file_path]') - Define qual o arquivo de template a ser complementado com o arquivo atual
          */
 
-
+        $pageSections = array();
         do {
             // Define o nome da última view carregada como sendo a atual, antes de processar
             // os dados (para comparar posteriormente)
@@ -49,35 +49,58 @@ class ViewManager
             // Posição contendo o caminho até a a view chamada, para buscar o template a partir do mesmo
             // diretório em que a view está
 
-            // Verifica se há 'sections' definidas na view
-            preg_match_all("/\@section[\s]*([\s\S]*?)@endsection/", $viewCode, $sections);
-            if (count($sections) > 0) {
-                $pageSections[] = $sections[1];
-            } // Posição em que são armazenados apenas os conteúdos das seções
 
-
+            // Verifica se há uma ou mais 'section' nomeadas na view
+            preg_match_all("/\@section\([\'|\"](.*?)[\'|\"]\)[\s]*([\s\S]*?)@endsection/", $viewCode, $sections);
+            // Caso haja ao menos uma section nomeada, organiza seus dados no vetor
+            if(count($sections[1]) > 0){
+                foreach($sections[1] as $j => $section){
+                    $pageSections[] = array('name' => $sections[1][$j], 'content' => $sections[2][$j]);
+                }
+            }
 
             if(count($extend[1]) > 0) { // Define o nome do template a ser carregado a seguir
                 $viewName = $extendPath . $extend;
             }
             // Obtém o HTML do template principal até aqui
 
-
             // Processa as seções das páginas, após o carregamento de todas as views
-            if(count($pageSections) > 0 && $lastView == $viewName){
-                // Vai substituindo de trás pra frente os valores (seção do template principal com o conteúdo
-                // do template secundário; template secundário com conteúdo do template terciário; ... com
-                // conteúdo da página passada inicialmente;
+            if(count($pageSections) > 0 && $lastView == $viewName) {
+                // Para cada seção encontrada, substitui o comando !!section('...')!! da view principal
+                // pelo equivalente da view que extende
 
-                for($j = count($pageSections)-1; $j >= 0; $j--){
-                    foreach ($pageSections[$j] as $section) {
-                        $viewCode = preg_replace('/!!section!!/', $section, $viewCode, 1);
+
+                // Regex para encontrar trechos como "!!section('...')!!", usada em ambos os foreach abaixo,
+                // na hora de fazer as substituições das seções
+                $sectionPattern = '/!!section\([\'|\"]' . $section['name'] . '[\'|\"]\)!!/';
+
+                // Analisa todas as seções obtidas e as substitui pelo conteúdo definido previamente
+                foreach ($pageSections as $section) {
+                    $viewCode = preg_replace(
+                        $sectionPattern,
+                        $section['content'],
+                        $viewCode,
+                        1
+                    );
+
+                    // [Verifica para cada seção se há uma seção interna]
+                    // Verifica se, após substituir, ainda existe uma nova seção, dentro do conteúdo vindo de outra
+                    // seção (Como "@section(x) ... !!section(y)!! ... @endsection")
+                    foreach ($pageSections as $section) {
+                        $viewCode = preg_replace(
+                            $sectionPattern,
+                            $section['content'],
+                            $viewCode,
+                            1
+                        );
                     }
+
                 }
             }
 
         } while($lastView != $viewName);
         // Repete enquanto a última view carregada não for igual à atual (será a última, a raiz)
+
 
         echo $viewCode;
         // Exibe o HTML carregado
