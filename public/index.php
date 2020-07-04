cade4a2a23ecfb8ae236365ae83a9ba67157cc36
Vinicius Baroni Soares <hiviniciusbs@gmail.com>
2020-07-04 15:30:11 -0300

    Adiciona comentário para identificação do framework

--- public/index.php
+++ public/index.php
@@ -1,3 +1,10 @@
 <?php
 
+/**
+ * @author Vinicius Baroni Soares <hiviniciusbs@gmail.com>
+ * @copyright 2020 - Luvi
+ * @link https://github.com/vinibs/luvi
+ * @license MIT License
+ */
+
 require_once __DIR__ . '/../src/core/bootstrap.php';
